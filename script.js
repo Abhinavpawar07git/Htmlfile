7129b6c3b072eb9d9ae0443cd09d3a1eb580b413
Abhinapawar07git <pawarabhinav37@gmail.com>
2023-09-03 23:39:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+  //callback function
+/*
+let a = [1,2,3,4,5]
+function b (callback) {
+    console.log("operation")
+    a.map(callback)
+}
+function cBFunction(number) {
+    console.log("Result " + number);
+}
+b(cBFunction) */
+ 
+// remove extra spaces
+ let a = " I am AbhinavPawar "
+  let b = a.trim();
+  console.log(b)
+  console.log( b.split(" ") .join('') ) 
+   
+  // reverse positive and negsative numbers
+     /*let a = 1235678;
+     let b = -2345678;
+     function doreverse(x) {
+     let c = Math.sign(x)*parseInt (
+     x.toString().split("").reverse.join(''));
+     console.log(c); } */
